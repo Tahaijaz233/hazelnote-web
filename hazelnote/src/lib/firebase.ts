c55d7cffe57ca8c62d57ba25d9f2831014767180
Taha Ijaz <140896806+Tahaijaz233@users.noreply.github.com>
2026-03-11 00:34:20 +0500

    Update firebase.ts

--- hazelnote/src/lib/firebase.ts
+++ hazelnote/src/lib/firebase.ts
@@ -1,7 +1,6 @@
 import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
-import { getStorage } from 'firebase/storage'; // Added Storage import
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
