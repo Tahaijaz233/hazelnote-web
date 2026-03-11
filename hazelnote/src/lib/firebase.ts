28836be1f873c49fa1e82225f5c2bdd301fd41a0
Taha Ijaz <140896806+Tahaijaz233@users.noreply.github.com>
2026-03-11 14:29:18 +0500

    Update firebase.ts

--- hazelnote/src/lib/firebase.ts
+++ hazelnote/src/lib/firebase.ts
@@ -7,8 +7,7 @@ const firebaseConfig = {
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.
-    NEXT_PUBLIC_FIREBASE_APP_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
@@ -16,4 +15,4 @@ const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-export { app, auth, db};
+export { app, auth, db };
